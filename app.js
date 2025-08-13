280e94430ac1d748ea7295c8fb685671754395cd
ivanvaic <ivan.vaic22@gmail.com>
2025-08-14 01:49:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Animacija pri učitavanju stranice
 document.addEventListener("DOMContentLoaded", function() {
+  // Efekat za fade-in animaciju headera
   const header = document.querySelector('header');
   header.classList.add('fade-in');
 });
